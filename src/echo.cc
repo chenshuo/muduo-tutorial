a2f17518a79442fabf4e0df06a1d744ca80fe932
Shuo Chen <chenshuo@chenshuo.com>
2011-05-29 13:44:48 +0800

    add examples of using Muduo as an installed library.

---
+++ src/echo.cc
@@ -0,0 +1,70 @@
+#include <muduo/net/TcpServer.h>
+
+#include <muduo/base/Logging.h>
+#include <muduo/base/Thread.h>
+#include <muduo/net/EventLoop.h>
+#include <muduo/net/InetAddress.h>
+
+#include <boost/bind.hpp>
+
+#include <utility>
+
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace muduo;
+using namespace muduo::net;
+
+class EchoServer
+{
+ public:
+  EchoServer(EventLoop* loop, const InetAddress& listenAddr)
+    : loop_(loop),
+      server_(loop, listenAddr, "EchoServer")
+  {
+    server_.setConnectionCallback(
+        boost::bind(&EchoServer::onConnection, this, _1));
+    server_.setMessageCallback(
+        boost::bind(&EchoServer::onMessage, this, _1, _2, _3));
+  }
+
+  void start()
+  {
+    server_.start();
+  }
+
+ private:
+  void onConnection(const TcpConnectionPtr& conn);
+
+  void onMessage(const TcpConnectionPtr& conn, Buffer* buf, Timestamp time);
+
+  EventLoop* loop_;
+  TcpServer server_;
+};
+
+void EchoServer::onConnection(const TcpConnectionPtr& conn)
+{
+  LOG_TRACE << conn->peerAddress().toHostPort() << " -> "
+            << conn->localAddress().toHostPort() << " is "
+	    << (conn->connected() ? "UP" : "DOWN");
+}
+
+void EchoServer::onMessage(const TcpConnectionPtr& conn, Buffer* buf, Timestamp time)
+{
+  string msg(buf->retrieveAsString());
+  LOG_TRACE << conn->name() << " recv " << msg.size() << " bytes at " << time.toString();
+  conn->send(msg);
+}
+
+int main(int argc, char* argv[])
+{
+  LOG_INFO << "pid = " << getpid() << ", tid = " << CurrentThread::tid();
+  EventLoop loop;
+  InetAddress listenAddr(2007);
+  EchoServer server(&loop, listenAddr);
+
+  server.start();
+
+  loop.loop();
+}
+
